8f16c511632091983e9fad3430a44c5da73d63bb
abreuit <diegoalves92@live.com>
2025-07-08 09:46:55 -0300

    feat: update rules 9090 - Prometheus

--- terraform/main.tf
+++ terraform/main.tf
@@ -71,6 +71,19 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+    name                       = "Prometheus9090"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9090"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
 
 #create VNIC
